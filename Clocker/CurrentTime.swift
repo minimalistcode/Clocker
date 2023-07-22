e781b74222cbef977e085605dc72290b5277c627
Jeff Zacharias <jeffzacharias@minimalistcode.com>
2023-07-22 09:48:23 -0400

    Refactored

---
+++ Clocker/CurrentTime.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentTime.swift
+//  Clocker
+//
+//  Created by Jeff Zacharias on 7/22/23.
+//
+
+import SwiftUI
+
+struct CurrentTime: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentTime()
+}
