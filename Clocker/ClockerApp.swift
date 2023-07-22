1f34e7f1a35392cd7aac9a01a0da2ca8ee1ab70f
Jeff Zacharias <jeffzacharias@minimalistcode.com>
2023-07-22 08:30:40 -0400

    Initial Commit

---
+++ Clocker/ClockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockerApp.swift
+//  Clocker
+//
+//  Created by Jeff Zacharias on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ClockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clocker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clocker
+//
+//  Created by Jeff Zacharias on 7/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
