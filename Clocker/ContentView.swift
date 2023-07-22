a2dcfd3c3c3c2d6c392e6d1ed6307c7f1b212068
Jeff Zacharias <jeffzacharias@minimalistcode.com>
2023-07-22 09:55:16 -0400

    Added time change

--- Clocker/ContentView.swift
+++ Clocker/ContentView.swift
@@ -5,45 +5,13 @@
 //  Created by Jeff Zacharias on 7/22/23.
 //
 
-// Features
-// Update every minute
-// Dark always
-// Dim with swipe up/down
-// Move evey move to prevent burn in
-
 import SwiftUI
 
 struct ContentView: View {
-	@Environment(\.colorScheme) private var colorScheme
-	
 	var body: some View {
-		HStack {
-			Spacer()
-			Text(currentTime())
-				.font(.system(size: 250))
-			Text(currentTimeAmPm())
-				.font(.system(size: 75))
-		}
-		.foregroundColor(colorScheme == .light ? .black : .white)
-		.background(colorScheme == .light ? .white : .black)
+		CurrentTime()
 	}
 }
-
-
-func currentTime() -> String {
-	let dateFormatter = DateFormatter()
-	dateFormatter.dateFormat = "H:mm"
-	dateFormatter.locale = Locale(identifier: "en_US")
-	return dateFormatter.string(from: Date())
-}
-
-func currentTimeAmPm() -> String {
-	let dateFormatter = DateFormatter()
-	dateFormatter.dateFormat = "a"
-	dateFormatter.locale = Locale(identifier: "en_US")
-	return dateFormatter.string(from: Date())
-}
-
 
 #Preview {
     ContentView()
--- Clocker/CurrentTime.swift
+++ Clocker/CurrentTime.swift
@@ -5,14 +5,52 @@
 //  Created by Jeff Zacharias on 7/22/23.
 //
 
+// Features
+// Update every minute
+// Dark always
+// Dim with swipe up/down
+// Move evey move to prevent burn in
+
 import SwiftUI
 
+
 struct CurrentTime: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@Environment(\.colorScheme) private var colorScheme
+	@State var timeString = ""
+	@State var amPmString = ""
+	let timer = Timer.publish(every: 60, on: .main, in: .common).autoconnect()
+	
+	var body: some View {
+		HStack {
+			Spacer()
+			Text(timeString)
+				.font(.system(size: 250))
+			Text(amPmString)
+				.font(.system(size: 75))
+		}
+		.foregroundColor(colorScheme == .light ? .black : .white)
+		.background(colorScheme == .light ? .white : .black)
+		.onAppear {
+			updateClock()
+		}
+		.onReceive(timer) { time in
+			updateClock()
+			print(Date())
+		}
+	}
+		
+	func updateClock() {
+		let dateFormatter = DateFormatter()
+		dateFormatter.locale = Locale(identifier: "en_US")
+		
+		dateFormatter.dateFormat = "H:mm"
+		timeString = dateFormatter.string(from: Date())
+		
+		dateFormatter.dateFormat = "a"
+		amPmString = dateFormatter.string(from: Date())
+	}
 }
 
 #Preview {
-    CurrentTime()
+	CurrentTime()
 }
