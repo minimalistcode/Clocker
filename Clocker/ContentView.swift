412df6608f475383ac5e1fc791334c91ec29e0eb
Jeff Zacharias <jeffzacharias@minimalistcode.com>
2023-07-22 09:34:34 -0400

    Initial Creation

--- Clocker/ContentView.swift
+++ Clocker/ContentView.swift
@@ -5,19 +5,45 @@
 //  Created by Jeff Zacharias on 7/22/23.
 //
 
+// Features
+// Update every minute
+// Dark always
+// Dim with swipe up/down
+// Move evey move to prevent burn in
+
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	@Environment(\.colorScheme) private var colorScheme
+	
+	var body: some View {
+		HStack {
+			Spacer()
+			Text(currentTime())
+				.font(.system(size: 250))
+			Text(currentTimeAmPm())
+				.font(.system(size: 75))
+		}
+		.foregroundColor(colorScheme == .light ? .black : .white)
+		.background(colorScheme == .light ? .white : .black)
+	}
 }
+
+
+func currentTime() -> String {
+	let dateFormatter = DateFormatter()
+	dateFormatter.dateFormat = "H:mm"
+	dateFormatter.locale = Locale(identifier: "en_US")
+	return dateFormatter.string(from: Date())
+}
+
+func currentTimeAmPm() -> String {
+	let dateFormatter = DateFormatter()
+	dateFormatter.dateFormat = "a"
+	dateFormatter.locale = Locale(identifier: "en_US")
+	return dateFormatter.string(from: Date())
+}
+
 
 #Preview {
     ContentView()
