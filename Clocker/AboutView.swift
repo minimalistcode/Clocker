c1cc42790dbb30cb69099a3e2faec207032bc95b
Jeff Zacharias <jeffzacharias@minimalistcode.com>
2023-07-22 20:28:40 -0400

    Added settings with About information

---
+++ Clocker/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  Clocker
+//
+//  Created by Jeff Zacharias on 7/22/23.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
